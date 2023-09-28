077f8f8960f28ba5bedd0a2a244afa70c23dfb22
samzyconcepts <samson.oderinwale@kibo.school>
2023-09-28 16:59:46 +0100

    about section, services section and faq section

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,32 @@
+import home1 from '../img/home1.png'
+
+const AboutSection = () => {
+    return (
+        <div>
+            <div className="description">
+                <div className="title">
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>
+                            your <span>dream</span> come
+                        </h2>
+                    </div>
+                    <div className="hide">
+                        <h2>true.</h2>
+                    </div>
+                </div>
+                <p>
+                    Contact us for any photography or videography ideas. We have professionals with
+                    amazing skills to help you achieve it.
+                </p>
+                <button>Contact Us</button>
+            </div>
+            <div className="image">
+                <img src={home1} alt="Guy with Camera" />
+            </div>
+        </div>
+    );
+};
+export default AboutSection;
