45fab949e9e51a2ec1abf309c9da91ef1c522c0d
samzyconcepts <samson.oderinwale@kibo.school>
2023-10-02 12:37:04 +0100

    adding toggle and animation to the faq section

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+// animation
+import { LayoutGroup } from "framer-motion";
 
 const FaqSection = () => {
     return (
@@ -7,38 +10,40 @@ const FaqSection = () => {
             <h2>
                 Any Questions <span>FAQ</span>
             </h2>
-            <div className="question">
-                <h4>How Do I Start</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4> Different Payment Method</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What Products Do You Offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <LayoutGroup>
+                <Toggle title="How Do I Start">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title="Daily Schedule?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title="Different Payment Method">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title="What Products Do You Offer?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores.
+                        </p>
+                    </div>
+                </Toggle>
+            </LayoutGroup>
         </Faq>
     );
 };
