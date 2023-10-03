acc535f1473480236737ee67d43288330185a0ac
samzyconcepts <samson.oderinwale@kibo.school>
2023-10-03 01:47:44 +0100

    nav menu active animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Nav = () => {
+    const { pathname } = useLocation();
     return (
         <StyledNav>
             <h1>
@@ -12,12 +14,27 @@ const Nav = () => {
             <ul>
                 <li>
                     <Link to="/">About Us</Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/" ? "50%" : '0%' }}
+                    />
                 </li>
                 <li>
                     <Link to="/work">Our Work</Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/work" ? "50%" : '0%' }}
+                    />
                 </li>
                 <li>
                     <Link to="/contact">Contact Us</Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/contact" ? "50%" : '0%' }}
+                    />
                 </li>
             </ul>
         </StyledNav>
@@ -57,18 +74,30 @@ const StyledNav = styled.nav`
     @media (max-width: 1300px) {
         flex-direction: column;
         padding: 2rem 1rem;
-        #logo{
+        #logo {
             display: inline-block;
-            margin: 1rem ;
+            margin: 1rem;
         }
         ul {
             padding: 2rem;
             justify-content: space-around;
             width: 100%;
-            li{
+            li {
                 padding: 0;
             }
         }
+    }
+`;
+
+const Line = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 5%;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+    @media (max-width: 1300px){
+        left: 0;
     }
 `;
 
